6d5613fea836babba05ffdc7521776ed88ffa9ec
hyeon45 <gahyeon4585@daum.net>
2024-10-08 13:06:54 +0900

    작은공과 큰 공을 10개씩 출력하여 이들이 각각 랜덤한 위치에서 떨어져 내림. 공이 잔디에 닿으면 멈춰서 이동하지 않음

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -29,6 +29,36 @@ class Boy:
         self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
 
 
+class BigBall:
+    def __init__(self):
+        self.x, self.y = random.randint(0, 800), 599
+        self.image = load_image('ball41x41.png')
+
+    def update(self):
+        if self.y <= 50:
+            self.y = 50
+        else:
+            self.y -= random.randint(0, 15)
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+
+class SmallBall:
+    def __init__(self):
+        self.x, self.y = random.randint(0, 800), 599
+        self.image = load_image('ball21x21.png')
+
+    def update(self):
+        if self.y <= 50:
+            self.y = 50
+        else:
+            self.y -= random.randint(0, 15)
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+
 def handle_events():
     global running
     events = get_events()
@@ -57,6 +87,8 @@ def reset_world():
     global running
     global grass
     global team
+    global big_ball
+    global small_ball
     global world
 
     running = True
@@ -68,6 +100,11 @@ def reset_world():
     team = [Boy() for i in range(11)] # 소년 11명의 팀이 생김
     world += team
 
+    big_ball = [BigBall() for i in range(10)]
+    world += big_ball
+
+    small_ball = [SmallBall() for i in range(10)]
+    world += small_ball
 
 open_canvas()
 
