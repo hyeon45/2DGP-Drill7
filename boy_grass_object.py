576c2e11c221b9542fd70d53318cf24e25c336cd
hyeon45 <gahyeon4585@daum.net>
2024-10-08 12:32:34 +0900

    잔디와 캐릭터를 생성하고 캐릭터가 왼쪽에서 오른쪽으로 쭉 이동을 한다. 기본적인 틀 구조를 완성하였다.

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,6 +1,32 @@
 from pico2d import *
 
+
 # Game object class here
+class Grass:
+    def __init__(self):
+        #생성자를 이용하여 객체의 초기 상태를 유지함
+        self.image = load_image('grass.png')
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = 0, 90
+        self.frame = 0
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame +1) %8
+        self.x += 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+
 
 def handle_events():
     global running
@@ -11,11 +37,42 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
+
+def update_world():
+    grass.update()
+    boy.update()
+    pass
+
+
+def render_world():
+    clear_canvas()
+    grass.draw()
+    boy.draw()
+    update_canvas()
+
+
+def reset_world():
+    global running
+    global grass
+    global boy
+
+    running = True
+    grass = Grass() # grass 함수 생성
+    boy = Boy()
+
+
 open_canvas()
 
 # initialization code
+reset_world()
 
 # game main loop code
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.05)
+
 
 # finalization code
 
